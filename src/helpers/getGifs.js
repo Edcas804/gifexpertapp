826ee6a9b6de86c21c64d1acad35571b1960bec8
edcas804 <edcasdevelopment@gmail.com>
2022-11-27 18:23:59 -0500

    update api to https

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -2,7 +2,7 @@ const api_key= '2EC869oghf14NRt1v0oVFEbz1iC3iwhF'
 const limit = 12
 
 export const getGifs= async ( category ) => {
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${encodeURI(category)}&limit=${limit}`
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${encodeURI(category)}&limit=${limit}`
     const res = await fetch(url)
     const { data }= await res.json()
     const gifs = data.map(item => {
